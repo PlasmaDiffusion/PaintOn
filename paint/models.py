0bfd60ae38ef14b38577db8b9de846f382bab83e
PlasmaDiffusion <Scott50000@gmail.com>
2019-12-01 18:26:31 -0500

    Update models.py

--- paint/models.py
+++ paint/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Layer(models.Model):
     #URL for saving and loading the image
-    url = models.URLField(default="")
+    url = models.URLField(max_length=10485760, default="")
 
     #Layer (0-9)
     layerNumber = models.IntegerField(default=0)
