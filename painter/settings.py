2509b78bf2a4aecd31174b810a17c8213a8499dc
PlasmaDiffusion <Scott50000@gmail.com>
2019-12-01 20:21:44 -0500

    Update settings.py

--- painter/settings.py
+++ painter/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-STATIC_URL = 'https://painton.herokuapp.com/static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
