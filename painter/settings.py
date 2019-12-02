eafaab8e8a7b161d521ba3e10b2f0ad02ec2a243
PlasmaDiffusion <Scott50000@gmail.com>
2019-12-01 20:34:34 -0500

    Static sucks

--- painter/settings.py
+++ painter/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'paint/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'paint/static'),
 )
 
 
